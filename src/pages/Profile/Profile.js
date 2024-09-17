f7855e34a1f384be9596c9d21317afe93f8bc9fd
Sofiacastilho <sofiarscastilho@gmail.com>
2024-09-17 13:44:48 -0300

    vagas e filtro

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -398,7 +398,7 @@ const Profile = () => {
       )}
 
       <div className="user-photos">
-        <h2 className="titulo">Vagas publicadas:</h2>
+        <h2 className="titulo">Suas vagas:</h2>
         {photos &&
           photos.map((photo) => (
             <div className="photo" key={photo._id}>
